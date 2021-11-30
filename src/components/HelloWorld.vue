f4f49892542c9f32c8800e58e95c2bfc0abd71d4
eduardobautistag <eduardo.bautista@delfosti.com>
2021-11-30 12:05:16 -0500

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Para probar Easy Auth de Azure, hacer clic 
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">aquí</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
